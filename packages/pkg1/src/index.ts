a5f4ccbd9540143eed47d93027e9c306fc1bb0cb
罗侠 <luoxia@supos.com>
2022-07-04 18:06:31 +0800

    feat: 修改包的组织名称

--- packages/pkg1/src/index.ts
+++ packages/pkg1/src/index.ts
@@ -4,7 +4,7 @@
  * @LastEditors: luoxia
  * @LastEditTime: 2022-07-04 14:50:38
  */
-import pkg2 from '@qftjs/monorepo2';
+import pkg2 from 'luoshenxia/monorepo2';
 
 function fun1() {
   console.log('I am package 1')
