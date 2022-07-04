9c033147a4310b5f6246c6f095e38123744d9e6c
罗侠 <luoxia@supos.com>
2022-07-04 19:20:09 +0800

    feat: 子包之间依赖

--- packages/pkg1/src/index.ts
+++ packages/pkg1/src/index.ts
@@ -2,11 +2,12 @@
  * @Author: luoxia
  * @Date: 2022-07-04 14:50:27
  * @LastEditors: luoxia
- * @LastEditTime: 2022-07-04 18:38:01
+ * @LastEditTime: 2022-07-04 19:19:33
  */
-// import pkg2 from '@lsxsuperman/monorepo2';
+import pkg2 from '@lsxsuperman/monorepo4';
 
 function fun1() {
+  pkg2();
   console.log('I am package 1')
 }
 
